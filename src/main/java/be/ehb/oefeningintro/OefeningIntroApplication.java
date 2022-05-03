4744cc3f2354265a253eeb9307364660389de933
david.van.steertegem <david.van.steertegem@ehb.be>
2022-05-03 12:20:07 +0200

    initial projectstructure

---
+++ src/main/java/be/ehb/oefeningintro/OefeningIntroApplication.java
@@ -0,0 +1,13 @@
+package be.ehb.oefeningintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OefeningIntroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OefeningIntroApplication.class, args);
+    }
+
+}
